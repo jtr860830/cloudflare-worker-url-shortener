d30ca3a2391acca57d193309f7c290bede742819
Josh Hsieh <josh.hsieh@linux.com>
2025-03-28 16:21:43 +0800

    chore: bump version

--- src/index.js
+++ src/index.js
@@ -9,18 +9,14 @@
  */
 
 export default {
-  async fetch(request, env) {
-    const alias = new URL(request.url)
-      .pathname
-      .substring(1);
-    const sheetApiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${env["GOOGLE_SHEET_ID"]}/values/A2:B${env["URL_MAX_NUM"] + 1}?key=${env["GOOGLE_SHEET_API_KEY"]}`;
-    const res = await fetch(sheetApiUrl);
-    const data = await res.json();
-    const row = data
-      .values
-      .find(el => el[0] === alias);
-    if (row === undefined) return new Response("Not found", { status: 404 });
-    const url = row[1];
-    return Response.redirect(url, 302);
-  },
+	async fetch(request, env) {
+		const alias = new URL(request.url).pathname.substring(1);
+		const sheetApiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${env["GOOGLE_SHEET_ID"]}/values/A2:B${env["URL_MAX_NUM"] + 1}?key=${env["GOOGLE_SHEET_API_KEY"]}`;
+		const res = await fetch(sheetApiUrl);
+		const data = await res.json();
+		const row = data.values.find((el) => el[0] === alias);
+		if (row === undefined) return new Response("Not found", { status: 404 });
+		const url = row[1];
+		return Response.redirect(url, 302);
+	},
 };
--- src/index.test.js
+++ src/index.test.js
@@ -2,20 +2,20 @@ import { unstable_dev } from "wrangler";
 import { describe, expect, it, beforeAll, afterAll } from "vitest";
 
 describe("Worker", () => {
-  let worker;
+	let worker;
 
-  beforeAll(async () => {
-    worker = await unstable_dev("src/index.js", {
-      experimental: { disableExperimentalWarning: true },
-    });
-  });
+	beforeAll(async () => {
+		worker = await unstable_dev("src/index.js", {
+			experimental: { disableExperimentalWarning: true },
+		});
+	});
 
-  afterAll(async () => {
-    await worker.stop();
-  });
+	afterAll(async () => {
+		await worker.stop();
+	});
 
-  it("should redirect to another site", async () => {
-    const res = await worker.fetch("", { redirect: "manual" });
-    expect(res.status).toEqual(302);
-  });
+	it("should redirect to another site", async () => {
+		const res = await worker.fetch("", { redirect: "manual" });
+		expect(res.status).toEqual(302);
+	});
 });
